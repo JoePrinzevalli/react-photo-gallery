bb829c0abf675afb767ec612467498dbf9bbf1e4
JoePrinzevalli <joeprinzevalli123@gmail.com>
2022-06-06 21:14:08 -0400

    fixing app.js and photolist.js for routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,27 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import axios from 'axios';
+// import axios from 'axios';
 
 // App Components 
 import Nav from './Nav'
 import Search from './Search';
 import PhotoList from './PhotoList';
-import apiKey from './config.js'
+// import apiKey from './config.js'
 
-class App extends Component   {
-
-
-  state = {
-      photos: [],
-      // loading: true
-    }
-
-  //Fetch Photos from flicker
-  performFetch = () => {
-    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=${apiKey}&per_page=24&format=json&nojsoncallback=1`)
-    .then(res => { 
-      this.setState({
-        photos: res.photos.photo
-      })
-    })
-    .catch(err => {
-      console.log('Error fetching and parsing data', err);
-    })
-  }
-  
-
-  render() {
-    console.log(this.state.photos)
+const App = () =>   {
    return (
     <BrowserRouter>
       <div className="container">
       <Search />
+      <Nav />
       <Routes>
-        <Route path='/' element={<Nav />} />
-        <Route path='/' element={<PhotoList data={this.state.photos}/>} />
+        <Route path='/cats' element={PhotoList} />
+        <Route path='/cats' element={PhotoList} />
+        <Route path='/cats' element={PhotoList} />
         </Routes>
       </div>
     </BrowserRouter>
    )
-  }
 };
 
 
--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import apiKey from './config.js'
 
 
-const PhotoList = props => {
+class PhotoList extends Component {
     
-    console.log(props);
+    state = {
+        photos: [],
+        // loading: true
+      }
+  
+    //Fetch Photos from flicker
+    performFetch = () => {
+      axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=${apiKey}&per_page=24&format=json&nojsoncallback=1`)
+      .then(res => { 
+        this.setState({
+          photos: res.photos.photo
+        })
+      })
+      .catch(err => {
+        console.log('Error fetching and parsing data', err);
+      })
+    }
     
+    render(props) {
+        console.log(props);
     return (
         <div className="photo-container">
             <h2>Results</h2>
         </div>
     )
+    }
 }
 
 
